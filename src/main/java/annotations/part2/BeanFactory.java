44a9688239d2b2befbec887b75ca47ed69cebf9f
Nanne Baars <nanneb@gmail.com>
2024-01-10 09:11:54 +0100

    Improvements

--- src/main/java/annotations/part2/BeanFactory.java
+++ src/main/java/annotations/part2/BeanFactory.java
@@ -1,17 +1,22 @@
 package annotations.part2;
 
 import annotations.helper.Helper;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BeanFactory {
-  private Map<Class, Object> beanRegistry;
+  private Map<Class, Object> beanRegistry = new HashMap<>();
 
   public BeanFactory() {
     List<? extends Class> classesInPackage =
         Helper.findClassesInPackage(this.getClass().getPackageName());
     // TODO check if class is annotated with @EnableTiming if yes, create a proxy for that class and
     // put it in the map
+  }
+
+  public void registerBean(Class clazz, Object proxy) {
+    beanRegistry.put(clazz, proxy);
   }
 
   public <T> T getBean(Class<T> clazz) {
--- src/main/java/annotations/part2/assignment.md
+++ src/main/java/annotations/part2/assignment.md
@@ -35,19 +35,3 @@ Helper method to get the annotation from a class:
 ```
 House.class.getAnnotation(EnableTiming.class);
 ```
-
-Making a generic class:
-
-```
-public class Cache<T> {
-    private final Map<String, T> cache = new HashMap<>();
-
-    public T get(String key) {
-        return cache.get(key);
-    }
-
-    public void put(String key, T value) {
-        cache.put(key, value);
-    }
-}
-```
